cee11216a9a05234357f9b133d1036d5990817ac
yanghuaiGit <2774584057@qq.com>
2019-11-08 14:00:43 +0800

    增加oauth模块

---
+++ oauth/src/test/java/com/skrshop/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrshop.oauth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
