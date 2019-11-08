3e640724857f2a111c745bb6c816899b50e0e72f
yanghuaiGit <2774584057@qq.com>
2019-11-08 15:29:53 +0800

    增加授权客户端模块

---
+++ oauthclient/src/main/java/com/skrshop/oauthclient/OauthclientApplication.java
@@ -0,0 +1,13 @@
+package com.skrshop.oauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthclientApplication.class, args);
+    }
+
+}
---
+++ oauthclient/src/test/java/com/skrshop/oauthclient/OauthclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrshop.oauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
