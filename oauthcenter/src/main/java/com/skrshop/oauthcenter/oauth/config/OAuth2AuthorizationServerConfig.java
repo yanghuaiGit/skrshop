cc7856e684b2e50003493c2cbe79bf65cc78db0d
yanghuaiGit <2774584057@qq.com>
2019-11-28 21:13:35 +0800

    增加oauth2

--- oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/OAuth2AuthorizationServerConfig.java
+++ oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/OAuth2AuthorizationServerConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Resource
+    private TokenStore tokenStore;
 //    @Resource
 //    private AuthenticationManager authenticationManager;
 //
@@ -28,6 +33,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore);
 //        endpoints.authenticationManager(authenticationManager)
 //                .userDetailsService(userDetailsService);
     }
