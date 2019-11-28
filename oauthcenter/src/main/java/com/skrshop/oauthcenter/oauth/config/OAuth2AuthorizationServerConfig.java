66a9be6bbbb10c58479064f3dd749062349c3ebf
yanghuaiGit <2774584057@qq.com>
2019-11-28 21:49:56 +0800

    增加oauth2

--- oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/OAuth2AuthorizationServerConfig.java
+++ oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/OAuth2AuthorizationServerConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Configuration
@@ -19,6 +23,9 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     @Autowired(required = false)
     private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Resource
+    private TokenEnhancer tokenEnhancer;
 //    @Resource
 //    private AuthenticationManager authenticationManager;
 //
@@ -30,17 +37,23 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
         //添加客户端信息
         clients.inMemory()                  // 使用in-memory存储客户端信息
                 .withClient("client")
-                .authorizedGrantTypes("authorization_code","refresh_token","password")
+                .authorizedGrantTypes("authorization_code", "refresh_token", "password")
                 .accessTokenValiditySeconds(7200)
-                .scopes("all","write","read")
+                .scopes("all", "write", "read")
                 .secret("123")// client_id
                 .redirectUris("http://www.baidu.com");
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        if(Objects.nonNull(jwtAccessTokenConverter)){
-            endpoints.accessTokenConverter(jwtAccessTokenConverter);
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)  {
+        if (Objects.nonNull(jwtAccessTokenConverter) && Objects.nonNull(tokenEnhancer)) {
+            TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+            List<TokenEnhancer> enhancers = new ArrayList<>(6);
+            enhancers.add(jwtAccessTokenConverter);
+            enhancers.add(tokenEnhancer);
+            tokenEnhancerChain.setTokenEnhancers(enhancers);
+            endpoints.tokenEnhancer(tokenEnhancerChain)
+                    .accessTokenConverter(jwtAccessTokenConverter);
         }
         endpoints.tokenStore(tokenStore);
 //        endpoints.authenticationManager(authenticationManager)
--- oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/TokenStoreConfig.java
+++ oauthcenter/src/main/java/com/skrshop/oauthcenter/oauth/config/TokenStoreConfig.java
@@ -1,19 +1,25 @@
 package com.skrshop.oauthcenter.oauth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class TokenStoreConfig {
 
-    @Autowired
+    @Resource
     private RedisConnectionFactory redisConnectionFactory;
 
     @Bean
@@ -36,6 +42,20 @@ public class TokenStoreConfig {
             JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
             accessTokenConverter.setSigningKey("test_key");//配置JWT使用的秘钥
             return accessTokenConverter;
+        }
+
+        /**
+         * 扩展jwt内容
+         */
+        @Bean
+        @ConditionalOnMissingBean(name = "jwtTokenEnhancer")
+        public TokenEnhancer jwtTokenEnhancer() {
+            return (accessToken, authentication) -> {
+                Map<String, Object> info = new HashMap<>(2);
+                info.put("company", "skrshop");
+                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(info);
+                return accessToken;
+            };
         }
     }
 }
