ce0542a966a653cd380001227c09e9c431449473
yanghuaiGit <2774584057@qq.com>
2019-12-02 18:12:50 +0800

    去除图像验证码

---
+++ security-core/src/main/java/com/skrshop/securitycore/properties/ImageCodeProperties.java
@@ -0,0 +1,18 @@
+package com.skrshop.securitycore.properties;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ImageCodeProperties extends CodeProperties {
+
+    private int width = 180;
+    private int height = 40;
+    // 验证码干扰线数
+    private int lineCount = 10;
+
+    public ImageCodeProperties() {
+        setCodeCount(4);
+    }
+}
--- security-core/src/main/java/com/skrshop/securitycore/properties/ValidateCodeProperties.java
+++ security-core/src/main/java/com/skrshop/securitycore/properties/ValidateCodeProperties.java
@@ -5,5 +5,7 @@ import lombok.Data;
 @Data
 public class ValidateCodeProperties {
 
+    private ImageCodeProperties image = new ImageCodeProperties();
+
     private CodeProperties sms = new CodeProperties();
 }
