e2c8018170a408fb4538d4ae60b50f3fffca1f26
yanghuaiGit <2774584057@qq.com>
2019-12-02 20:37:45 +0800

    增加获取验证码端点

--- security-core/src/main/java/com/skrshop/securitycore/validate/code/ValidateCodeType.java
+++ security-core/src/main/java/com/skrshop/securitycore/validate/code/ValidateCodeType.java
@@ -3,6 +3,9 @@ package com.skrshop.securitycore.validate.code;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * @author 李建珍
  * @date 2019/3/24
@@ -13,8 +16,14 @@ public enum ValidateCodeType {
     SMS("sms"),
     IMAGE("image");
 
-
     private String validType;
 
+    public static Optional<String> findType(String type) {
+        return Arrays.stream(ValidateCodeType.values())
+                .map(ValidateCodeType::getValidType)
+                .filter(item -> item.equals(type))
+                .findFirst()
+                ;
+    }
 
 }
