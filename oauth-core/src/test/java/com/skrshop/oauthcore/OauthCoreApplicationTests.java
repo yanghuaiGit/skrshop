27f90fb6cede6262c397378910240ef97ee58dde
yanghuaiGit <2774584057@qq.com>
2019-12-02 14:45:12 +0800

    增加oauth-core核心包

---
+++ oauth-core/src/test/java/com/skrshop/oauthcore/OauthCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrshop.oauthcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
