9dfd0c8b0d2eebb092942c68d7a871cacf295611
yanghuaiGit <2774584057@qq.com>
2019-12-05 10:50:39 +0800

    重构oauth-center

--- mall-resource/src/main/java/com/skrshop/mallresource/config/OauthResourceConfig.java
+++ mall-resource/src/main/java/com/skrshop/mallresource/config/OauthResourceConfig.java
@@ -1,21 +1,122 @@
 package com.skrshop.mallresource.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableResourceServer
+@Slf4j
 public class OauthResourceConfig extends ResourceServerConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests()
-               .anyRequest()
-               .authenticated()
-               .and()
-               .requestMatchers()
-               .antMatchers("/api/**");
+
+    @Resource
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+    @Bean
+    @ConditionalOnMissingBean(TokenStore.class)
+    public TokenStore jwtTokenStore() {
+        log.info("tokenstore: {}","jwtTokenStore");
+        return new JwtTokenStore(jwtAccessTokenConverter);
     }
+
+    @Bean
+    @ConditionalOnMissingBean(JwtAccessTokenConverter.class)
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        log.info("jwtAccessTokenConverter: {}","jwtAccessTokenConverter");
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("test_key");//配置JWT使用的秘钥accessTokenConverter
+        return accessTokenConverter;
+    }
+
+    /**
+     * 扩展jwt内容
+     */
+    @Bean
+    @ConditionalOnMissingBean(TokenEnhancer.class)
+    public TokenEnhancer jwtTokenEnhancer() {
+        return (accessToken, authentication) -> {
+            Map<String, Object> info = new HashMap<>(2);
+            info.put("company", "skrshop");
+            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(info);
+            return accessToken;
+        };
+    }
+
+
+//@Bean
+//    public TokenStore tokenStore() {
+//
+//        return new JwtTokenStore(jwtAccessTokenConverter());
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean(JwtAccessTokenConverter.class)
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+//        accessTokenConverter.setSigningKey("test_key");//配置JWT使用的秘钥
+//        return accessTokenConverter;
+//    }
+
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.anonymous()
+                .disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+//                .and()
+//                .requestMatchers()
+//                .antMatchers("/api/**")
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(new OAuth2AccessDeniedHandler())
+        ;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenServices(tokenServices());
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+
+        defaultTokenServices.setTokenStore(jwtTokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+
+//    @Primary
+//    @Bean
+//    public RemoteTokenServices tokenServices() {
+//        final RemoteTokenServices tokenService = new RemoteTokenServices();
+//        //通过服务注册与发现获取到对应的域名
+//        tokenService.setCheckTokenEndpointUrl("http://localhost:8080/oauth/check_token");
+//        tokenService.setClientId("c1");
+//        tokenService.setClientSecret("secret");
+//        return tokenService;
+//    }
+
 }
