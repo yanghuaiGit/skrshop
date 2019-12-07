88d4e6eabaf0d062f95872ddc662be94e9974deb
yanghuaiGit <2774584057@qq.com>
2019-12-07 11:12:50 +0800

    增加zookeeper模块

---
+++ support-zookeeper/src/test/java/com/skrshop/supportzookeeper/SupportZookeeperApplicationTests.java
@@ -0,0 +1,14 @@
+package com.skrshop.supportzookeeper;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
