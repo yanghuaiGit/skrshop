fd2b81b8120c46b039568f7a9f38e0fe76e1759b
yanghuaiGit <2774584057@qq.com>
2019-12-10 15:28:05 +0800

    resttemplate

--- common/src/main/java/com/skrshop/common/config/RestTemplateConfig.java
+++ common/src/main/java/com/skrshop/common/config/RestTemplateConfig.java
@@ -1,16 +1,29 @@
 package com.skrshop.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
+@Slf4j
 public class RestTemplateConfig {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> list = new ArrayList<>();
+        list.add((request, body, execution) -> {
+            log.info("restTemplate拦截器:当前请求的URL是【{}】", request.getURI().toString());
+            return execution.execute(request, body);
+        });
+        restTemplate.setInterceptors(list);
+        return restTemplate;
     }
 
 }
