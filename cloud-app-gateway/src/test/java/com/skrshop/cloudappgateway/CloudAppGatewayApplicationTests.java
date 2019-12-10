f8bbfd6abdbd0d7d3993a0a08558e934c4575cb1
yanghuaiGit <2774584057@qq.com>
2019-12-10 15:58:58 +0800

    增加app网关模块

---
+++ cloud-app-gateway/src/test/java/com/skrshop/cloudappgateway/CloudAppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrshop.cloudappgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAppGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
