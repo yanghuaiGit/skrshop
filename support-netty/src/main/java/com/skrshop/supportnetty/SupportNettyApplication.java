1ba6fda82669b67ea98e3a6adfc1bc6301bc2f4b
yanghuaiGit <2774584057@qq.com>
2019-12-18 17:52:06 +0800

    代码优化

---
+++ support-netty/src/main/java/com/skrshop/supportnetty/SupportNettyApplication.java
@@ -0,0 +1,13 @@
+package com.skrshop.supportnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportNettyApplication.class, args);
+    }
+
+}
