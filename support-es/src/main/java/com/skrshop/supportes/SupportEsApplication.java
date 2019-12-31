8fc752f528d1877df3a86b0617ebb58026d750ef
yanghuaiGit <2774584057@qq.com>
2019-12-31 17:45:35 +0800

    增加es模块

---
+++ support-es/src/main/java/com/skrshop/supportes/SupportEsApplication.java
@@ -0,0 +1,13 @@
+package com.skrshop.supportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportEsApplication.class, args);
+    }
+
+}
